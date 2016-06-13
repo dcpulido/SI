7aabd2fcbf6233861fbea7450099e5c042b69707
dcpulido <dcpulido@esei.uvigo.es>
2016-06-13 17:51:18 +0200

    practica 3 arreglada

---
+++ practica3/Practica3/QueensEnv.java
@@ -0,0 +1,278 @@
+import jason.asSyntax.*;
+import jason.environment.Environment;
+import jason.environment.grid.GridWorldModel;
+import jason.environment.grid.GridWorldView;
+import jason.environment.grid.Location;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.*;
+
+import java.io.*;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+
+public class QueensEnv extends Environment {
+
+    public static final int GSize  =   8; // grid size
+    public static final int EMPTY  =   8; // empty cell code in grid model
+    public static final int ATACK  =  16; // empty cell code in grid model
+    public static final int HOLE   =  32; // hole in a cell code in grid model
+    public static final int QUEEN  =  64; // queen code in grid model
+    public static final int BLOCK  = 128; // block in a cell code in grid model
+
+    public static final Term    pq = Literal.parseLiteral("put(queen)");
+
+    static Logger logger = Logger.getLogger(QueensEnv.class.getName());
+
+    private QueensModel model;
+    private QueensView  view;
+	private int queensPlaced;
+	
+    
+    @Override
+    public void init(String[] args) {
+        model = new QueensModel();
+        view  = new QueensView(model);
+        model.setView(view);
+        updatePercepts();
+		queensPlaced = 0;
+    }
+    
+    @Override
+    public boolean executeAction(String ag, Structure action) {
+        logger.info(ag+" doing: "+ action);
+        try {
+			if (action.getFunctor().equals("move_towards")) {
+                int x = (int)((NumberTerm)action.getTerm(0)).solve();
+                int y = (int)((NumberTerm)action.getTerm(1)).solve();
+                model.moveTowards(x,y);
+            } else if (action.getFunctor().equals("queen")) {
+                	int x = (int)((NumberTerm)action.getTerm(0)).solve();
+					int y = (int)((NumberTerm)action.getTerm(1)).solve();
+					model.putQueen(x,y);
+            	} else if (action.getFunctor().equals("freeBB")) {
+                		int x = (int)((NumberTerm)action.getTerm(0)).solve();
+						int y = (int)((NumberTerm)action.getTerm(1)).solve();
+                		model.freeAttack(x,y);
+            		} else if (action.getFunctor().equals("clean")) {
+                				int x = (int)((NumberTerm)action.getTerm(0)).solve();
+								int y = (int)((NumberTerm)action.getTerm(1)).solve();
+								model.clean(x,y);
+            				} else if (action.getFunctor().equals("block")) {
+										int x = (int)((NumberTerm)action.getTerm(0)).solve();
+										int y = (int)((NumberTerm)action.getTerm(1)).solve();
+										model.putBlock(x,y);
+								} else {
+										return false;
+										}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        updatePercepts();
+
+        try {
+            Thread.sleep(200);
+        } catch (Exception e) {}
+        informAgsEnvironmentChanged();
+        return true;
+    }
+    
+    /** creates the agents perception based on the QueensModel */
+    void updatePercepts() {
+        //clearPercepts();
+        
+        //Location r1Loc = model.getAgPos(0);
+        //Location r2Loc = model.getAgPos(1);
+        
+        //Literal pos1 = Literal.parseLiteral("pos(r1," + r1Loc.x + "," + r1Loc.y + ")");
+		
+		Literal sizeBB = Literal.parseLiteral("size(" + GSize + ")");
+
+        //addPercept(pos1);
+		
+		addPercept(sizeBB);
+    }
+
+    class QueensModel extends GridWorldModel {
+        
+        public static final int MErr = 2; // max error in pick garb
+        int nerr; // number of tries of pick garb
+        boolean r1HasGarb = false; // whether r1 is carrying garbage or not
+
+        Random random = new Random(System.currentTimeMillis());
+
+        private QueensModel() {
+            super(GSize, GSize, 3); //Now the environment play with 3 agents
+            
+            // initial location of agents
+            try {
+                setAgPos(0, 0, 0);
+                Location r2Loc = new Location(GSize/2, GSize/2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+ 			for (int x = 0; x < GSize; x++) {
+                for (int y = 0; y < GSize; y++) {
+                    add(EMPTY,x,y);
+                }
+            };
+			
+        }
+        
+		
+        void moveTowards(int x, int y) throws Exception {
+            Location r1 = getAgPos(0);
+			r1.x = x;
+			r1.y = y;
+            setAgPos(0, r1);
+        }
+        
+		void putQueen(int x, int y) throws Exception {
+			Literal turn = Literal.parseLiteral("player(" + queensPlaced%2 + ")");
+			removePercept(turn);
+            add(QUEEN, x, y);
+			Literal queen = Literal.parseLiteral("queen(" + x + "," + y + ")");
+			addPercept(queen);
+			queensPlaced++;
+			turn = Literal.parseLiteral("player(" + queensPlaced%2 + ")");
+			addPercept(turn);
+			cellAttacked(x,y);
+        }
+		
+		void putBlock(int x, int y) throws Exception {
+            add(BLOCK, x, y);
+			Literal block = Literal.parseLiteral("block(" + x + "," + y + ")");
+			addPercept(block);
+        }
+		
+        void cellAttacked(int x, int y) {
+ 			Literal attackCell;
+			int col;
+			for (int i = 0; i < GSize; i++) {
+				if (i != x & isFree(ATACK, i, y) ) {
+					add(ATACK, i, y);
+					attackCell = Literal.parseLiteral("attack(" + i + "," + y + ")");
+					addPercept(attackCell);
+				};
+				if (i != y) {
+					if (isFree(ATACK, x, i)) {
+						add(ATACK, x, i);
+						attackCell = Literal.parseLiteral("attack(" + x + "," + i + ")");
+					addPercept(attackCell);};
+					col= x-i+y;
+					if ((0 <= col) & (GSize > col) & isFree(ATACK, col, i)) {
+						add(ATACK, col, i);
+						attackCell = Literal.parseLiteral("attack(" + col + "," + i + ")");
+						addPercept(attackCell);
+					};
+					col= x+i-y;
+					if ((GSize > col) & (0 <= col) & isFree(ATACK, col, i)){
+						add(ATACK, col, i);
+						attackCell = Literal.parseLiteral("attack(" + col + "," + i + ")");
+						addPercept(attackCell);
+					};
+				}
+			};
+			try {
+				Thread.sleep(100);
+       		} catch (Exception e) {};
+
+        }
+        
+		void clean(int x, int y) throws Exception {
+            remove(QUEEN,x,y);
+			Literal queen = Literal.parseLiteral("queen(" + x + "," + y + ")");
+			removePercept(queen);
+        }
+        
+		void freeAttack(int x, int y) throws Exception {
+            remove(ATACK,x,y);
+			Literal attackedCell = Literal.parseLiteral("attack(" + x + "," + y + ")");
+			removePercept(attackedCell);
+        }
+    }
+    
+    class QueensView extends GridWorldView {
+	
+		public QueensView(QueensModel model) {
+            super(model, "Queen's World", 400);
+			this.model = model;
+			defaultFont = new Font("Arial", Font.BOLD, 18); // change default font
+			setVisible(true);
+			Graphics g = this.getCanvas().getGraphics();
+			model.setView(this);
+			//repaint();
+        }
+
+		/** draw application objects */
+        @Override
+        public void draw(Graphics g, int x, int y, int object) {
+            switch (object) {
+                case QueensEnv.EMPTY: drawEmpty(g, x, y);  break;
+                case QueensEnv.QUEEN: drawQueen(g, x, y);  break;
+                case QueensEnv.ATACK: drawAtack(g, x, y);  break;
+                case QueensEnv.BLOCK: drawObstacle(g, x, y);  break;
+            }
+        }
+		
+        /** draw application objects */
+        @Override
+        public void drawEmpty(Graphics g, int x, int y) {
+			if ((x+y)%2==0){
+				g.setColor(Color.yellow);
+			} else {
+				g.setColor(Color.cyan);
+			};
+			g.fillRect(x * cellSizeW + 1, y * cellSizeH+1, cellSizeW-1, cellSizeH-1);
+			g.drawRect(x * cellSizeW, y * cellSizeH, cellSizeW, cellSizeH);
+		}
+
+        @Override
+        public void drawAgent(Graphics g, int x, int y, Color c, int id) {
+			if ((x+y)%2==0){
+				c = Color.black;
+			} else {
+				c = Color.white;
+			};
+         }
+
+		public void drawAtack(Graphics g, int x, int y) {
+			if (queensPlaced%2==1){
+				g.setColor(Color.white);
+			} else {
+				g.setColor(Color.black);
+			};
+			g.fillOval(x * cellSizeW + 2, y * cellSizeH + 2, cellSizeW - 4, cellSizeH - 4);
+        }
+		
+        public void drawQueen(Graphics g, int x, int y) {
+			BufferedImage bqImg = null;
+			BufferedImage wqImg = null;
+			try {
+				bqImg = ImageIO.read(new File("img/bq.png"));
+				wqImg = ImageIO.read(new File("img/wq.png"));
+				} catch (IOException e) {
+			}
+			
+			//if ((x+y)%2==0){ 
+			if (queensPlaced%2==1){ 
+				g.drawImage(bqImg, x * cellSizeW + 1, y * cellSizeH - 1, null);
+			} else {
+				g.drawImage(wqImg, x * cellSizeW + 1, y * cellSizeH - 1, null);
+			};
+			
+        }
+
+    }    
+}
---
+++ practica3/Practica3/r1.asl
@@ -0,0 +1,141 @@
+/* Initial beliefs */
+
+qfor(Y,Y,L,R):- mirafila(Y,Y,L,R,Y).
+
+qfor(X,Y,L,K):-	X1=X+1 &
+						concat(MF,R,K)&
+						mirafila(X,Y,L,MF,Y) &
+						qfor(X1,Y,L,R).
+
+mirafila(X,0,L,[],S):-	ataca([X,0],L,S).
+mirafila(X,0,_,[[X,0]],_).
+mirafila(X,Y,L,MF,S):-	Y1=Y-1 &
+						ataca([X,Y],L,S) &
+						mirafila(X,Y1,L,MF,S).
+mirafila(X,Y,L,[[X,Y]|MF],S):-	Y1=Y-1 &
+								mirafila(X,Y1,L,MF,S).
+
+ataca([],[],_).
+ataca([X,_],[[X,_]|_],_).
+ataca([_,Y],[[_,Y]|_],_).
+
+ataca(Q,[Car|Cdr],P):-	ataca(Q,Cdr,P)|
+						atacaDiag(Q,P,[Car|Cdr]).
+
+atacaDiag([X1,X2],P,[[C1,C2]|R]):-	comprueba(X1,X2,P,[[C1,C2]|R])|
+								    nextlvl([X1,X2],P,[[C1,C2]|R]).
+
+nextlvl([X1,X2],P,[[C1,C2]|R]):-P>0 &
+								P1=P-1 & 
+								atacaDiag([X1,X2],P1,[[C1,C2]|R]).
+
+
+
+comprueba(X1,X2,P,[[C1,C2]|R]):-comprueba1(X1,X2,P,C1,C2)|
+						  		comprueba2(X1,X2,P,C1,C2)|
+						  		comprueba3(X1,X2,P,C1,C2)|
+			   			  		comprueba4(X1,X2,P,C1,C2)|
+						  		comprueba5(X1,X2,P,C1,C2)|
+						  		comprueba(X1,X2,P,R).
+
+
+comprueba1(X1,X2,P,C1,C2):-	X1=C1+P & X2=C2+P.
+comprueba2(X1,X2,P,C1,C2):-	X1=C1-P & X2=C2+P.
+comprueba3(X1,X2,P,C1,C2):-	X1=C1+P & X2=C2-P.
+comprueba4(X1,X2,P,C1,C2):-	X1=C1-P & X2=C2-P.
+comprueba5(X1,X2,_,C1,C2):-	X1=C1 & X2=C2.						
+
+monta([],[]).
+monta([Car|Cdr],[[X,Y]|L]):- despieza(Car,X,Y) &
+							 monta(Cdr,L).
+						   
+despieza([],[],[]).
+despieza(q(X,Y),X,Y).
+
+miembro(X,[X|_]).
+miembro(X,[_|Y]):- miembro(X,Y).
+
+concat([], Cs, Cs).
+concat([A|As],Bs,[A|Cs]):-
+          concat(As, Bs, Cs).
+		  
+diferencia([],_,[]).
+diferencia([A|B],K,M):- miembro(A,K)& diferencia(B,K,M).
+diferencia([A|B],K,[A|M]):- not(miembro(A,K))& diferencia(B,K,M).
+
+elemrandom(L,N,E):- get(L,N,E).
+						
+get([Car|Cdr],0,Car).
+get([Car|Cdr],N,X):- N1=N-1 &
+				get(Cdr,N1,X).
+		  
+longitud([],0).
+longitud([_|T],N):-longitud(T,N0) & N=N0 + 1.
+
+parset([X,Y],X,Y).
+
+igual(X,X).
+
+bloqueo([X,Y],[[X,Y]|_]).
+bloqueo([X,Y],[_|Cdr]):-bloqueo([X,Y],Cdr).
+
+/* Initial goal */
+!start.
++!start: true <- +player(0).
+/* Plans */
++player(N):playAs(N)<-
+			!play(N).
++player(N) : playAs(M) & not N==M <- .wait(300); .print("No es mi turno.").
++!play(P) <-
+	!jugadas(Njugadas);
+	!tamTablero(Tam);
+	.findall(q(C,D),queen(C,D),L);
+	?monta(L,Lista);
+	!tablero(X,Y);
+	?qfor(X,Y,Lista,ListaA);
+
+	.findall(q(C,D),block(C,D),LB);
+	?monta(LB,ListaB);
+	.print(ListaB);
+	
+	
+	if(.empty(ListaB)){?igual(ListaA,ListaPosibles);}
+	else{?diferencia(ListaA,ListaB,ListaPosibles);}
+
+	if(.empty(ListaPosibles)){
+		.print(Fin);
+	}else{
+		.print("Sin Limpiar: ",ListaPosibles);
+		?longitud(ListaPosibles,Long);
+		!aleatorio(Long-1,Num);
+		?elemrandom(ListaPosibles,Num,Ele);
+		?parset(Ele,Elx,Ely);
+		.findall(q(C,D),block(C,D),LK);
+		?monta(LK,Lis);
+		if(bloqueo([Elx,Ely],Lis)){
+			!play(P);
+		}
+		else{
+			queen(Elx,Ely);
+		}
+	}.
+	
+
+
++!jugadas(Njugadas):size(N)<-
+Njugadas=N/2.
+	
++!aleatorio(Long,Num)<-
+.random(X);
+Y = X*Long;
+Num= math.round(Y).
+
+	
++!tablero(X,Y):size(N)<-
+	X=0;
+	Y=N-1.
+	
++!tamTablero(Tam):size(N)<-
+	Tam=N.
+
+
